f6960d54156b9d1770f18845832caafe3e0eb1f2
wenlong <530675800@qq.com>
2025-01-06 15:27:47 +0800

    feat: 优化请求 添加取消功能

--- web/src/api/request.ts
+++ web/src/api/request.ts
@@ -1,13 +1,38 @@
 import axios from 'axios'
+// import router from '@src/router/index'
 
-const request = axios.create({
-  // baseURL: "/api",
+// pending requests
+const pendingRequests: { [key: string]: AbortController } = {}
+
+const removePendingRequest = (url: string | undefined, abort = false): void => {
+  // check if pendingRequests contains our request URL
+  if (url && pendingRequests[url]) {
+    // if we want to abort ongoing call, abort it
+    if (abort) {
+      pendingRequests[url]?.abort()
+    }
+    // remove the request URL from pending requests
+    delete pendingRequests[url]
+  }
+}
+
+export const request = axios.create({
   timeout: 5000
 })
 
 // 请求拦截器
 request.interceptors.request.use(
   config => {
+    let token = sessionStorage.getItem('token')
+
+    if (token) config.headers.token = token
+
+    removePendingRequest(config.url, true)
+
+    const abortController = new AbortController()
+    config.signal = abortController.signal
+    pendingRequests[config.url!] = abortController
+
     return config
   },
   err => {
@@ -18,7 +43,20 @@ request.interceptors.request.use(
 // 响应拦截器
 request.interceptors.response.use(
   res => {
-    return res.data
+    removePendingRequest(URL.parse(res.request.responseURL)?.pathname)
+
+    if (res.data.code !== 200) {
+      // // 登录失效
+      // if (res.data.code === 403) {
+      //   router.push('/login')
+      //   sessionStorage.removeItem('token')
+      //   return
+      // }
+
+      return Promise.reject(res.data)
+    } else {
+      return res.data.data
+    }
   },
   err => {
     return Promise.reject(err)
@@ -26,3 +64,10 @@ request.interceptors.response.use(
 )
 
 export default request
+
+export interface ResError {
+  code: number
+  message: string
+  data: any
+  ok: boolean
+}
