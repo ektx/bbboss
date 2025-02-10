162897fedee2d2009d6b67f2f395f74451a6e824
wenlong <530675800@qq.com>
2025-02-10 13:13:09 +0800

    Update index.vue

--- web/src/views/home/index.vue
+++ web/src/views/home/index.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="main-view">
     <h1>{{ title }}</h1>
-    <RouterLink to="/login">Login</RouterLink>
+
+    <div class="menu-list">
+      <RouterLink to="/home">家</RouterLink>
+      <RouterLink to="/newArticle">拼音</RouterLink>
+      <RouterLink to="/newArticle">算术</RouterLink>
+      <RouterLink to="/newArticle">英语</RouterLink>
+      <RouterLink to="/login">登录</RouterLink>
+    </div>
+
+    <div class="content">
+      <router-view />
+    </div>
   </div>
 </template>
 
@@ -9,7 +20,7 @@
 import { onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
 
-const title = ref('Home')
+const title = ref('家')
 const router = useRouter()
 
 if (!sessionStorage.getItem('token')) {
